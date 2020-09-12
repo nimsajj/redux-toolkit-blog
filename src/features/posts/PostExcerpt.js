62beff5fe830fa3a1be6c62255d6e0ce1448f186
nimsajj <nimsaj.dev@gmail.com>
2020-09-12 18:45:29 +0200

    Displaying Loading State

---
+++ src/features/posts/PostExcerpt.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
+
+export const PostExcerpt = ({ post }) => (
+  <article className="post-excerpt" key={post.id}>
+    <h3>{post.title}</h3>
+    <p>{post.content.substring(0, 100)}</p>
+    <div>
+      <PostAuthor userId={post.user} />
+      <TimeAgo timestamp={post.date} />
+    </div>
+    <Link to={`/posts/${post.id}`} className="button muted-button">
+      View Post
+    </Link>
+    &nbsp;
+    <Link to={`/editPost/${post.id}`} className="button">
+      Edit Post
+    </Link>
+    <ReactionButtons post={post} />
+  </article>
+)
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,9 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { PostAuthor } from './PostAuthor'
-import { TimeAgo } from './TimeAgo'
-import { ReactionButtons } from './ReactionButtons'
+import { PostExcerpt } from './PostExcerpt'
+
 import { selectAllPosts, fetchPosts } from './postsSlice'
 
 export const PostsList = () => {
@@ -11,6 +9,7 @@ export const PostsList = () => {
   const posts = useSelector(selectAllPosts)
 
   const postStatus = useSelector((state) => state.posts.status)
+  const error = useSelector((state) => state.posts.error)
 
   useEffect(() => {
     if (postStatus === 'idle') {
@@ -18,33 +17,25 @@ export const PostsList = () => {
     }
   }, [postStatus, dispatch])
 
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date))
+  let content
 
-  const renderedPosts = orderedPosts.map((post) => (
-    <article className="post-excerpt" key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content.substring(0, 100)}</p>
-      <div>
-        <PostAuthor userId={post.user} />
-        <TimeAgo timestamp={post.date} />
-      </div>
-      <Link to={`/posts/${post.id}`} className="button muted-button">
-        View Post
-      </Link>
-      &nbsp;
-      <Link to={`/editPost/${post.id}`} className="button">
-        Edit Post
-      </Link>
-      <ReactionButtons post={post} />
-    </article>
-  ))
+  if (postStatus === 'loading') {
+    content = <div className="loader">Loading...</div>
+  } else if (postStatus === 'succeeded') {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date))
+    content = orderedPosts.map((post) => (
+      <PostExcerpt key={post.id} post={post} />
+    ))
+  } else if (postStatus === 'failed') {
+    content = <div>{error}</div>
+  }
 
   return (
     <section>
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
